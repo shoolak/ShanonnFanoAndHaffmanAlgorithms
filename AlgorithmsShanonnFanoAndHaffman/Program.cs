f4d93c478a9f313febc1caea91f5ec9864fbfb68
Oleh <dzonoleg@gmail.com>
2023-11-17 11:23:11 +0200

    first commit nothing exciting

---
+++ AlgorithmsShanonnFanoAndHaffman/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("This is Algorithm of Shanonn-Fano and Haffman for compress and decompress file or input string");
+    }
+}
